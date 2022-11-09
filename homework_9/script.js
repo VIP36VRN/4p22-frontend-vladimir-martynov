ff1dcb070201e9e2f379f028641a82d5345deb8c
notyou <mac@MacBook.local>
2022-11-09 20:20:19 +0300

    Reload homework_9

--- homework_9/script.js
+++ homework_9/script.js
@@ -1,13 +1,8 @@
 'use strict'
-
-
-// ------------- call function in code -------------- //
-
 const fibonacci = function makeFibonacciFunction() {
     let previousNumber = 0;
     let nextNumber = 1;
     
-
     return function () {
         let result = previousNumber + nextNumber;
         console.log(previousNumber);
