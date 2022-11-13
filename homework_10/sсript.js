89bd202177d54bc127892beb28612538869a975d
notyou <mac@MacBook.local>
2022-11-13 21:29:37 +0300

    add homework_10

---
+++ homework_10/sсript.js
@@ -0,0 +1,41 @@
+'use strict';
+
+async function getresponse() {
+        let promise = await fetch(`https://reqres.in/api/users?per_page=12`);
+
+        let result = await promise.json()
+
+        let surname = `Фамилии: `;
+
+    result.data.forEach((element, index, array) => {
+
+        if (index !== (array.length - 1)) {
+            surname += element.last_name + `, `;} else
+
+        if (index === (array.length - 1)) {
+            surname += element.last_name + `.`;} 
+    });
+    
+    console.log (surname);
+
+    console.log(Object.keys(result.data[`0`]).join(`, `));
+      
+    const findElement = result.data.filter(item => 
+        item.last_name[0] === 'F');
+
+    console.log(findElement); 
+
+    let accGlobal = `Наша база содержит данные следующих пользователей: `
+        const userBase = result.data.reduce((accLocation, item, index, array ) => {
+
+            let massage = accLocation + (item.last_name + ` ` + item.first_name);
+
+        if (index !==(array.length - 1)) {
+            massage += `, `;}
+            return massage;
+        }, accGlobal);
+
+    console.log(userBase);
+
+};
+getresponse();
